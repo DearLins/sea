ddbede22ac8df6b6188bb4aa25793555d35b6c51
kong1981 <xiaokongzi@163.com>
2021-04-20 23:23:52 +0800

    添加配置文件

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"sea/conf"
 	"sea/controllers"
 )
-
+var config *conf.Config
 func main()  {
+	config, err := conf.ParseConfig("conf/config.json")
+	fmt.Println(config.AppHost)
+	fmt.Println(err)
 	route := gin.Default()
 	route.GET("/index", controllers.Index)
 	route.GET("/redis", controllers.Redis)
